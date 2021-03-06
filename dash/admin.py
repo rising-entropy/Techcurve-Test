96b343a9e4458873c3493ad4f46d70783fa5c492
Devang Kamble <risingentropy20@gmail.com>
2021-03-06 13:00:04 +0530

    in admin reg models

--- dash/admin.py
+++ dash/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(Expenses)
+admin.site.register(Invoice)
+admin.site.register(Revenues)
+admin.site.register(BankBalance)
